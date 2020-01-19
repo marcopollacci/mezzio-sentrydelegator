1cd7a52f10229dd7df5d39c6b51479bae17ea077
Marco <pollacci@gmail.com>
2020-01-19 11:02:09 +0100

    First

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-mezzio-sentrydelegator
+# Sentry Delegator for Mezzio - Lamisas Framework
+
+## Install by Composer
+```
+composer require marcopollacci/mezzio-sentrydelegator
+```
+
+## Usage
+
+1) Set at your config file with sentry token and dsn (if no config given, no exception send to sentry). Es:
+```php
+return [
+    'sentry' => [
+        'dsn' => 'https://<your url>>/<your id>',
+        'environment' => 'produzione' #optional
+    ]
+];
+```
+2) Enjoy it :D!
+
+## Additional usage in try / catch statament
+
+Code Example
+```php
+        try {
+            throw new \Exception('something');
+        }catch(\Exception $e){
+            \Sentry\captureException($e);
+            /* something else you want */
+        }
+```
---
+++ src/ConfigProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MarcoPollacci\MezzioSentryDelegator;
+
+use Laminas\Stratigility\Middleware\ErrorHandler;
+use MarcoPollacci\MezzioSentryDelegator\Delegators\SentryDelegator;
+
+/**
+ * The configuration provider for the MarcoPollacci Tipsandtrick module
+ *
+ * @see https://docs.zendframework.com/zend-component-installer/
+ */
+class ConfigProvider
+{
+    /**
+     * Returns the configuration array
+     *
+     * To add a bit of a structure, each section is defined in a separate
+     * method which returns an array with its configuration.
+     */
+    public function __invoke() : array
+    {
+        return [
+            'dependencies' => $this->getDependencies()
+        ];
+    }
+
+    /**
+     * Returns the container dependencies
+     */
+    public function getDependencies() : array
+    {
+        return [
+            'invokables' => [
+            ],
+            'factories'  => [
+                Middleware\SentryMiddleware::class => Middleware\SentryMiddlewareFactory::class
+            ],
+            'delegators' => [
+                ErrorHandler::class => [
+                    SentryDelegator::class
+                ],
+            ],
+        ];
+    }
+}
